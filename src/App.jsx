8d83e3cb935beb46c75e86da6d75cb86c593f69e
Nilesh <nvrangari@gmail.com>
2024-04-19 12:44:04 +0530

    app,jsx edited

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import './App.css'
+import Content from './components/Content'
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
 
 function App() {
-
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <div className='bg-[#191b1f] bg-cover px-10 flex-col justify-center items-center'>
+      <Navbar/>
+      <Content/>
+      <Footer/>
+    </div>
   )
 }
 
 export default App
+
