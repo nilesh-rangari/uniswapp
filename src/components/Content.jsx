4eb76177f139981de76a9994c9112f9c5d11d394
Nilesh <nvrangari@gmail.com>
2024-04-24 11:44:54 +0530

    heading text adjusted

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -60,7 +60,7 @@ function Content() {
             <div className="bg-[#191b1f] h-screen w-full mx-auto flex justify-center mb-16 lg:mb-16">
                 <div className="absolute h-screen w-full bg-cover" style={{ backgroundImage: `url(${bgStars})` }}>
                         <div className="absolute inset-0 flex flex-col justify-end items-start px-6 sm:px-14 lg:px-20">
-                            <h1 className="text-white text-left text-[42px] sm:text-[56px] sm:w-full text-wrap mb-5">
+                            <h1 className="text-white text-left text-[42px] flex flex-col sm:flex-row sm:text-[56px] sm:w-full text-wrap mb-5">
                                 <span className="font-light mr-3 my-0">
                                     UNISWAP
                                 </span>
